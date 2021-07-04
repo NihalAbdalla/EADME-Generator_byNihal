e9fc2b17ca3a5e2321b992145293911bbdb162ea
Nihal Abdalla <miz46769@gmail.com>
2021-07-03 21:26:23 -0400

    generate readMeGenerator.js update index.js with questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,52 @@
-// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+var readMeGenerator = require("./ReadMeGenerator");
+var fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is name of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Provide a description of your project?",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "What license you choose for your application?",
+        name: "license",
+        choices: [
+            "GVL-GP",
+            "MIT",
+            ""
+        ]
+    },
+    {
+        type: "input",
+        message: "What installation your command run?",
+        name: "installation",
+        default: 'npm i'
+    },
+    {
+        type: "input",
+        message: "What test you run on Node.js?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the name of your Github?",
+        name: "github",
+    },
+    
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
---
+++ Develop/utils/ReadMeGenerator.js
@@ -0,0 +1,41 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== "return a license badge"){
+    return 'true'
+  }
+  if (license ==== "no license found"){
+    return '',
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "return a license link"){
+    return 'true'
+  }
+  if (license ==== "no license link found"){
+    return '',
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "return a license README") {
+    return 'true'
+  }
+  if (license ==== "no license README found"){
+    return '',
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function readMeGenerator(data) {
+  return `# ${data.title}
+  ${renderLicenseBadge(license.data)}
+`;
+}
+
+module.exports = readMeGenerator;
